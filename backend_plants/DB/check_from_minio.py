02f1880f89f0897ff25040b14906282488f114de
dasha124 <dashakonekb@gmail.com>
2025-02-25 07:49:06 +0300

    все порты - в env

--- backend_plants/DB/check_from_minio.py
+++ backend_plants/DB/check_from_minio.py
@@ -3,6 +3,7 @@ import environ
 env = environ.Env()
 
 HOST_BACKEND = env('HOST_BACKEND')
-REGISTER_URL = f'http://{HOST_BACKEND}:8100/api/from_minio/'
+PORT=env('PORT')
+REGISTER_URL = f'http://{HOST_BACKEND}:{PORT}/api/from_minio/'
 
 print("Процедура приверки минио завершена.")
